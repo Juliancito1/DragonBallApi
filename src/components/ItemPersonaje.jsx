8b4ee79243d71827377932d1088b2695c71c5446
Julian <julianser@hotmail.com.ar>
2024-10-10 00:59:11 -0300

    Se hizo un rediseño de la pagina principal y se agregaron estilos como cambiar el scrollbar y ademas de superponer las cards por encima de una imagen

--- src/components/ItemPersonaje.jsx
+++ src/components/ItemPersonaje.jsx
@@ -3,17 +3,24 @@ import { Card, Col, Button } from 'react-bootstrap';
 
 const ItemPersonaje = ({personaje}) => {
   return (
-    <Col lg={3}>
-      <Card className='my-5'>
-        <Card.Img variant="top" src={personaje.image} className='imgPersonaje' />
-        <Card.Body className='bg-black text-white'>
-          <Card.Title>{personaje.name}</Card.Title>
-          <Card.Text className='text-truncate'>
-            {personaje.description}
-          </Card.Text>
-          <Button variant="primary">Ver Más</Button>
-        </Card.Body>
-      </Card>
+    <Col md={4} lg={3} className='mb-5'>
+      <div className='position-relative'>
+        <img
+          src="https://i.pinimg.com/originals/a5/f9/a2/a5f9a2eb5c0bfb1f66988696e1f31334.png"
+          alt="esfera de dragon"
+          className='imgEsfera'
+        />
+        <Card className="my-5 cardFondo position-absolute">
+          <Card.Img
+            variant="top"
+            src={personaje.image}
+            className="imgPersonaje"
+          />
+          <Card.Body className="text-white">
+            <Card.Title className='text-center'>{personaje.name}</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
     </Col>
   );
 }
